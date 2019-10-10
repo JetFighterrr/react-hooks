e9742ab34d24c1dba689029cdf4e4941184deef0
soigoshin@gmail.com <Ibkj7852>
2019-10-09 22:51:43 -0700

    vg234

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import {Provider, Store} from './store/useStore';
+
+
 
 ReactDOM.render(
-    <Provider>
+    <Provider store = {Store}>
         <App />
     </Provider>,
     document.getElementById('root')
